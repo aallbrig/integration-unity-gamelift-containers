7c72aedcb4e107f6b191592e822472c70991ddf0
Andrew Allbright <aallbrig@gmail.com>
2024-05-23 15:12:27 -0400

    offline scene

--- unity/Multiplayer_TowerDefense/Assets/Controllers/MenuController.cs
+++ unity/Multiplayer_TowerDefense/Assets/Controllers/MenuController.cs
@@ -1,5 +1,8 @@
+using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 namespace Controllers
@@ -7,9 +10,15 @@ namespace Controllers
     public class MenuController : MonoBehaviour
     {
         [SerializeField] private UIDocument uiDocument;
-        [SerializeField] private string clientConnectToLocalhostButtonName = "client-connect-localhost-game-server";
-        public UnityEvent onClientConnectToLocalhost;
+        // [SerializeField] private string connectButtonQuery = "client-connect-localhost-game-server";
+        [SerializeField] private string btnQuickPlayQuery = "btn_quick_play";
+        [SerializeField] private string btnFindAGameQuery = "btn_find_a_game";
+        [SerializeField] private string btnServerBrowserQuery = "btn_server_browser";
+        [SerializeField] private string btnSettingsQuery = "btn_settings";
+        [SerializeField] private NetworkManager networkManager;
+        // public UnityEvent onClientConnectToLocalhost;
         private Button _clientConnectToLocalhostButton;
+        private int _currentSelectionIndex = 0;
         private void Awake()
         {
             uiDocument ??= GetComponent<UIDocument>();
@@ -19,19 +28,37 @@ namespace Controllers
                 enabled = false;
                 return;
             }
-            _clientConnectToLocalhostButton = uiDocument.rootVisualElement.Q<Button>(clientConnectToLocalhostButtonName);
+            networkManager ??= FindObjectOfType<NetworkManager>();
+            uiDocument.rootVisualElement.Q<Button>(btnQuickPlayQuery);
+            uiDocument.rootVisualElement.Q<Button>(btnFindAGameQuery);
+            uiDocument.rootVisualElement.Q<Button>(btnServerBrowserQuery);
+            uiDocument.rootVisualElement.Q<Button>(btnSettingsQuery);
+            uiDocument.rootVisualElement.Q<VisualElement>("[focusable=true]");
+            /*
+            _clientConnectToLocalhostButton = uiDocument.rootVisualElement.Q<Button>(connectButtonQuery);
             if (_clientConnectToLocalhostButton == null)
             {
-                Debug.LogError($"{name} | no 'connect to localhost' button found (using name: {clientConnectToLocalhostButtonName})");
+                Debug.LogError($"{name} | no 'connect to localhost' button found (using query: {connectButtonQuery})");
                 enabled = false;
                 return;
             }
             _clientConnectToLocalhostButton.clicked += ConnectToLocalhostGameServer;
+            */
         }
+        /*
         private void ConnectToLocalhostGameServer()
         {
             Debug.Log($"{name} | connecting to localhost game server...");
             onClientConnectToLocalhost?.Invoke();
+        }
+        */
+        private void OnNavigate(InputValue inputValue)
+        {
+            Debug.Log($"{name} | On navigate {inputValue.Get<Vector2>()} focused element: {uiDocument.rootVisualElement.focusController.focusedElement}");
+        }
+        private void OnSubmit(InputValue inputValue)
+        {
+            Debug.Log($"{name} | On submit {inputValue.isPressed}");
         }
     }
 }
--- unity/Multiplayer_TowerDefense/Assets/Scripts/GameServer/MultiplayerNetworkManager.cs
+++ unity/Multiplayer_TowerDefense/Assets/Scripts/GameServer/MultiplayerNetworkManager.cs
@@ -5,18 +5,21 @@ namespace GameServer
 {
     public class MultiplayerNetworkManager : NetworkManager
     {
-        public GameServerConfiguration config;
         public override void OnStartServer()
         {
             Debug.Log($"{name} | On Start Server");
         }
+        /*
         public override void OnClientConnect()
         {
             Debug.Log($"{name} | On Client Connect");
+            // base.OnClientConnect();
         }
+        */
         public override void OnClientDisconnect()
         {
             Debug.Log($"{name} | On Client Disconnect");
+            base.OnClientDisconnect();
         }
     }
 }
